8e5f0dbb2bba573d72edaadb64014c354ab0cd17
Kowalskiii189 <yojithpanduru@gmail.com>
2026-01-16 17:05:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Reveal on scroll
+const reveals = document.querySelectorAll(".reveal");
+
+const revealOnScroll = () => {
+  const windowHeight = window.innerHeight;
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if (top < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+};
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
